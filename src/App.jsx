761f3fad3328d69d3f8e1644336ccc235d977f78
Schweppes2007 <wave10102544@gmail.com>
2024-12-11 17:18:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import ReactDom from "react-dom";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold underline">สวัสดีครับ</h1>
+      <></>
+    </div>
+  );
+}
