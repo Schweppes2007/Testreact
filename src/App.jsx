d14f86e2d89391149d715296194b8da47a33bbdd
Schweppes2007 <wave10102544@gmail.com>
2024-12-13 17:11:56 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="bg-red-500">
-      <h1 className="text-3xl font-bold underline">สวัสดีครับ</h1>
+      <h1 className="text-3xl font-bold underline">สวัสดีครับเวฟ</h1>
       <></>
     </div>
   );
